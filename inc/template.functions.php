bc1a94c0faabd08d9a5e08d9e2ee282a7e622446
Manuel <frama9125@gmail.com>
2026-02-07 22:12:56 -0500

    version update

--- inc/template.functions.php
+++ inc/template.functions.php
@@ -14,7 +14,7 @@
  * http://opensource.org/licenses/MIT
  */
  
- define('THEME_VER', '1.0.44');
+ define('THEME_VER', '1.0.45');
  define('__PATH_TEMPLATE_MODULES__', __PATH_TEMPLATE__ . 'inc/modules/');
 
 function templateBuildNavbar() {
